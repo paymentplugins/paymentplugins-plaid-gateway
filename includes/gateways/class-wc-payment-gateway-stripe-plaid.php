bd47a2634e795117f5bea7e1753a59d915fdf00e
Clayton Rogers <mr.clayton@paymentplugins.com>
2022-09-21 00:16:11 -0700

    update

--- includes/gateways/class-wc-payment-gateway-stripe-plaid.php
+++ includes/gateways/class-wc-payment-gateway-stripe-plaid.php
@@ -393,9 +393,9 @@ if ( class_exists( 'WC_Payment_Gateway_Stripe' ) ) {
 			return wp_script_is( $scripts->get_handle( 'ach' ) );
 		}
 
-		/*public function admin_nav_tab( $tabs ) {
+		public function admin_nav_tab( $tabs ) {
 			return $tabs;
-		}*/
+		}
 
 	}
 
